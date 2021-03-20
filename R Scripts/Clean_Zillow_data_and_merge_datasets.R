1a3b70f6fee10df66d73d3e4a61db3d72e253243
Joey-Herrera <joseph.herrera@utexas.edu>
2021-03-20 14:03:34 -0500

    Update Clean_Zillow_data_and_merge_datasets.R

--- R Scripts/Clean_Zillow_data_and_merge_datasets.R
+++ R Scripts/Clean_Zillow_data_and_merge_datasets.R
@@ -65,31 +65,31 @@ Zillow_long
 Zillow_long <- mutate(Zillow_long, YEAR = format(as.Date(Zillow_long$date, format="%Y-%m-%d"),"%Y"))
 
 ### Condense data frame by RegionName and year to lower the overall number of observations
-region_list_zillow <- c("Amarillo, TX", "Austin, TX", 
-                        "Beaumont, TX", "Brownsville, TX",
-                        "Corpus Christi", "College Station",
-                        "Dallas-Fort Worth, TX", "El Paso, TX",
-                        "Houston, TX", "Laredo, TX",
-                        "Lubbock, TX", 
-                        "Midland, TX", "Odessa, TX", "San Angelo, TX",
-                        "San Antonio, TX",
-                        "Tyler, TX", "Waco, TX", "Wichita Falls, TX")
+#region_list_zillow <- c("Amarillo, TX", "Austin, TX", 
+ #                       "Beaumont, TX", "Brownsville, TX",
+  #                      "Corpus Christi", "College Station",
+   #                     "Dallas-Fort Worth, TX", "El Paso, TX",
+    #                    "Houston, TX", "Laredo, TX",
+     #                   "Lubbock, TX", 
+      #                  "Midland, TX", "Odessa, TX", "San Angelo, TX",
+       #                 "San Antonio, TX",
+        #                "Tyler, TX", "Waco, TX", "Wichita Falls, TX")
 
-Zillow_long_condensed = Zillow_long %>%
-  filter(RegionName %in% region_list_zillow) %>%
-  group_by(RegionName, YEAR) %>%
-  summarise( avg_price = mean(price))
+#Zillow_long_condensed = Zillow_long %>%
+ # filter(RegionName %in% region_list_zillow) %>%
+  #group_by(RegionName, YEAR) %>%
+  #summarise( avg_price = mean(price))
 
 # Take out observations in 2020 and 2021
-extra_years = c( '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013',
+#extra_years = c( '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013',
                 '2014', '2015', '2016', '2017', '2018', '2019')
 
-Zillow_long_condensed = Zillow_long_condensed %>%
-  filter(YEAR %in% extra_years)
+#Zillow_long_condensed = Zillow_long_condensed %>%
+ # filter(YEAR %in% extra_years)
   
 
 ### Code for joining the ACS data and the Zillow data into a single dataset using year as the common variable
-TX_housing <- merge(IPUMS_Travis , Zillow_long_condensed, by="YEAR")
+#TX_housing <- merge(IPUMS_Texas_filtered_2013 , Zillow_long_condensed, by= c("YEAR", "RegionName"))
 
 
 
