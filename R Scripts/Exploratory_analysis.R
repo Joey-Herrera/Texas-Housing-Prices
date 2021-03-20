e5f204c8648c0c714b962cc6b81cb6c3906a9eae
Joey-Herrera <joseph.herrera@utexas.edu>
2021-03-20 13:30:30 -0500

    Update Exploratory_analysis.R

--- R Scripts/Exploratory_analysis.R
+++ R Scripts/Exploratory_analysis.R
@@ -2,9 +2,7 @@
 
 ggplot(data = TX_housing) +
   geom_line(aes(x=YEAR, y=mean_age)) +
-  scale_x_discrete(breaks = c(), labels=c("2006","2007","2008", "2009", "2010", "2011",
-                            "2012", "2013", "2014", "2015", "2016", "2017",
-                            "2018", "2019")) +
+ scale_x_continuous(breaks = seq(2006, 2019, 4)) +
   facet_wrap(~RegionName) +
   xlab("Year (2006 to 2019)") +
   ylab("Average age")
