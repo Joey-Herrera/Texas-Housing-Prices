87cfbcf3597480ddf96b610fa5c66c1feb95360e
Joey-Herrera <joseph.herrera@utexas.edu>
2021-03-26 21:28:16 -0500

    Update Exploratory_analysis.R

--- R Scripts/Exploratory_analysis.R
+++ R Scripts/Exploratory_analysis.R
@@ -26,3 +26,20 @@ ggplot(data = TX_housing) +
   xlab("Year (2006 to 2019)") +
   ylab("Average housing Price") +
   ggtitle("Average Housing Prices in TX Metro Regions Over Time")
+
+#Focus on the change in housing prices in Austin by year
+#Filter housing to only include average housing cost in housing
+TX_housing_Austin = TX_housing %>%
+  filter(RegionName == "Austin, TX")
+
+ggplot(data = TX_housing_Austin) +
+  geom_col(aes(x=YEAR, y=avg_price, fill = YEAR)) +
+  scale_x_continuous(breaks = seq(2006, 2019, 2)) +
+  scale_y_continuous(breaks = seq(0, 350000, 25000), labels = scales::comma) +
+  facet_wrap(~RegionName) +
+  xlab("Year (2006 to 2019)") +
+  ylab("Average housing Price") +
+  ggtitle("Average Housing Prices in Austin Over Time") +
+  theme_linedraw()
+
+
